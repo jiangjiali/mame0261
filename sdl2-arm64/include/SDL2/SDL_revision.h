3545691f46405a47d716077888c9117923dbc044
蒋佳李 <jiali.jiang@dragonplus.com>
2024-01-16 16:55:36 +0800

    删除

--- sdl2-arm64/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-#define SDL_VENDOR_INFO "Github Workflow"
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.5-g2ce037d (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.28.5-g2ce037d"
-#endif
